8a4819639f1b94fcf24028c80914b0e9c1b17bc4
Roger Yu <roger-yu@outlook.com>
2025-05-02 00:04:37 +1000

    Add overlapping and pre-embeddings

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import random
+import pandas as pd
 import numpy as np
 import torch
 import joblib
 from datetime import datetime
-from train import train_sakt_with_early_stopping
+from train import evaluate_test, train_sakt_with_early_stopping
 from models.sakt import SAKT, SAKTWithAdditivePreEmbeddings, SAKTConfig
 from dataloader import XES3G5MDataModule, XES3G5MDataModuleConfig
 from logging_config import get_logger
@@ -38,6 +39,17 @@ def parse_args():
         help="If set, the model will be trained in learning-curve mode",
     )
     parser.add_argument(
+        "--overlap_size",
+        type=int,
+        default=None,
+        help="If set, the training set will be extended to include overlapping sequences",
+    )
+    parser.add_argument(
+        "--pretrained_model_dir",
+        type=str,
+        help="If set, the model will evaluate the test set using {model_dir}_best_model.pth",
+    )
+    parser.add_argument(
         "--pre_embedding_list",
         nargs="+",
         type=str,
@@ -55,8 +67,22 @@ def parse_args():
 
 
 def get_model(
-    model_name: str, config: SAKTConfig, pre_embeddings: dict[str, torch.Tensor] | None
+    model_name: str, config: SAKTConfig, pre_embeddings: dict[str, torch.Tensor] | None, pretrained_model_dir: str | None = None
 ) -> torch.nn.Module:
+    """Get the model based on the model name. Or load a pretrained model from the model_dir.
+
+    Args:
+        model_name (str): The name of the model to train.
+        config (SAKTConfig): The configuration for the SAKT model.
+        pre_embeddings (dict[str, torch.Tensor] | None): The pre-embeddings for the model. Defaults to None.
+        model_dir (str | None, optional): The directory containing `best_model.pth`. Defaults to None.
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        torch.nn.Module: _description_
+    """
     if model_name == "sakt":
         model = SAKT(
             emb_dim=config.emb_dim,
@@ -76,8 +102,47 @@ def get_model(
         )
     else:
         raise ValueError(f"Unknown model name: {model_name}")
+    if pretrained_model_dir:
+        logger.info("Loading model from %s", pretrained_model_dir)
+        model_path = Path(pretrained_model_dir) / "best_model.pth"
+        if not model_path.exists():
+            logger.error("Model path does not exist: %s", model_path)
+            raise FileNotFoundError(f"Model path does not exist: {model_path}")
+        model.load_state_dict(torch.load(model_path))
+        logger.info("Model loaded successfully")
     return model
 
+
+def get_pre_embeddings(
+    pre_embedding_list: list[str],
+    data_module: XES3G5MDataModule,
+) -> dict[str, torch.Tensor] | None:
+    """Get the pre-embeddings for the model.
+
+    Args:
+        pre_embedding_list (list[str] | None): The list of pre-embeddings to use.
+        data_module (XES3G5MDataModule): The data module.
+
+    Returns:
+        dict[str, torch.Tensor] | None: The pre-embeddings for the model.
+    """
+    if pre_embedding_list is None:
+        return None
+    pre_embeddings = {}
+    for emb_name in args.pre_embedding_list or []:
+        try:
+            embeddings = getattr(data_module, f"{emb_name}_embeddings")
+        except AttributeError:
+            logger.error(
+                f"Pre-embedding {emb_name} not found in data module. Available: `question`, `concept`"
+            )
+            continue
+        embeddings = torch.tensor(embeddings, dtype=torch.float32)
+        pre_embeddings[emb_name] = embeddings
+        logger.info(
+            f"Pre-embedding {emb_name} loaded with shape: {pre_embeddings[emb_name].shape}"
+        )
+    return pre_embeddings
 
 def main(args=None):
     """Main function to train the SAKT model."""
@@ -94,10 +159,34 @@ def main(args=None):
         data_module = XES3G5MDataModule(config=XES3G5MDataModuleConfig())
         logger.info("Preparing data...")
         data_module.prepare_data()
-        logger.info("Setting up data...")
-        data_module.setup(stage="fit")
-        data_module.setup(stage="test")
         joblib.dump(data_module, data_path)
+
+    pre_embeddings = get_pre_embeddings(
+        pre_embedding_list=args.pre_embedding_list,
+        data_module=data_module,
+    )
+
+    # Evaluate the test set
+    pretrained_model_dir = args.pretrained_model_dir
+    if pretrained_model_dir:
+        logger.info("Evaluating test set using model from %s", pretrained_model_dir)
+        model_path = Path(pretrained_model_dir) / "best_model.pth"
+        if not model_path.exists():
+            logger.error("Model path does not exist: %s", model_path)
+            return
+        model = get_model(
+            model_name=args.model,
+            config=SAKTConfig(),
+            pre_embeddings=pre_embeddings,
+            pretrained_model_dir=args.pretrained_model_dir,
+        )
+
+        model.to(device)
+        data_module.setup(stage="test",)
+        test_dataloader = data_module.test_dataloader()
+        logger.info("Evaluating test set...")
+        evaluate_test(model, test_dataloader, device)
+        return
 
     if args.learning_curve:
         logger.info("Using learning-curve mode")
@@ -109,31 +198,28 @@ def main(args=None):
     # Add loop to train on different training set sizes
     for training_folds in training_folds_list:
         folds_name = "_".join(str(f) for f in training_folds)
-        log_dir = Path("logs") / "sakt" / f"{args.model}_{folds_name}_{now_str}"
+        log_dir = Path("logs") / "sakt" / f"{args.run_name}_{folds_name}_{now_str}"
         log_dir.mkdir(parents=True, exist_ok=True)
-        artifact_dir = Path("artifacts") / "sakt" / f"{folds_name}_{now_str}"
+        artifact_dir = (
+            Path("artifacts") / "sakt" / f"{args.run_name}_{folds_name}_{now_str}"
+        )
         artifact_dir.mkdir(parents=True, exist_ok=True)
-        data_module.setup(stage="fit", training_folds=training_folds)
+        logger.info(
+            "Setting up data: stage: %s, training_folds: %s, overlap_size: %s",
+            "fit",
+            training_folds,
+            args.overlap_size,
+        )
+        data_module.setup(
+            stage="fit", training_folds=training_folds, overlap_size=args.overlap_size
+        )
         logger.info("Training on folds: %s", training_folds)
 
         train_dataloader = data_module.train_dataloader()
         val_dataloader = data_module.val_dataloader()
         logger.info("Train dataloader and val dataloader created.")
         # Passing the pre-embeddings (if any) to the model
-        pre_embeddings = {}
-        for emb_name in args.pre_embedding_list or []:
-            try:
-                embeddings = getattr(data_module, f"{emb_name}_embeddings")
-            except AttributeError:
-                logger.error(
-                    f"Pre-embedding {emb_name} not found in data module. Available: `question`, `concept`"
-                )
-                continue
-            embeddings = torch.tensor(embeddings, dtype=torch.float32)
-            pre_embeddings[emb_name] = embeddings
-            logger.info(
-                f"Pre-embedding {emb_name} loaded with shape: {pre_embeddings[emb_name].shape}"
-            )
+        
 
         model = get_model(
             model_name=args.model,
@@ -143,7 +229,7 @@ def main(args=None):
         logger.info("Model initialized.")
         logger.info("Training model...")
         logger.info("Training on %d samples", len(train_dataloader.dataset))
-        model, best_val_loss, best_epoch = train_sakt_with_early_stopping(
+        model, best_val_loss, best_epoch, probs = train_sakt_with_early_stopping(
             model=model,
             train_loader=train_dataloader,
             val_loader=val_dataloader,
@@ -154,9 +240,12 @@ def main(args=None):
             patience=10,
         )
         logger.info("Training completed.")
-        model_path = artifact_dir / f"{args.model}_best_model.pth"
+        model_path = artifact_dir / f"best_model.pth"
         torch.save(model.state_dict(), model_path)
         logger.info("Model saved to %s", model_path)
+        probs_path = artifact_dir / f"probs.joblib"
+        joblib.dump(probs, probs_path)
+        logger.info("Probabilities saved to %s", probs_path)
 
 
 if __name__ == "__main__":
