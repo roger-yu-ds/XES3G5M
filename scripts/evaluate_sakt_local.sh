1d01d54efac4fe71a2cb15d7880086528a727fa4
Roger Yu <roger-yu@outlook.com>
2025-05-02 00:05:10 +1000

    Add and update scripts

--- scripts/evaluate_sakt_local.sh
+++ scripts/evaluate_sakt_local.sh
@@ -1,12 +1,10 @@
 export PYTHONPATH=$(pwd)
 export CUDA_LAUNCH_BLOCKING=1
-# MODEL=sakt
 MODEL=sakt_with_additive_pre_embeddings
-RUN_NAME=sakt_with_additive_question_pre_embeddings_overlapping
+PRETRAINED_MODEL_DIR=artifacts/sakt/sakt_with_additive_question_pre_embeddings_0_1_2_3_2025-04-29_22-37-13
 PRE_EMBEDDING_LIST=(question)
-OVERLAP_SIZE=100
+
 python src/main.py \
     --model=$MODEL \
     --pre_embedding_list ${PRE_EMBEDDING_LIST[@]} \
-    --run_name=$RUN_NAME \
-    --overlap_size=$OVERLAP_SIZE
+    --pretrained_model_dir=$PRETRAINED_MODEL_DIR
