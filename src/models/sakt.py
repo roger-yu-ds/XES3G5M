Fix bug in masking interaction_ids

src/models/sakt.py
@@ -328,8 +328,8 @@ class SAKTWithAdditivePreEmbeddings(Module):
         interaction_ids = question_ids + self.num_questions * responses
         interaction_ids = torch.where(
             condition=selectmasks == -1,
-            input=interaction_ids,
-            other=torch.full_like(interaction_ids, self.interaction_padding_idx),
+            input=torch.full_like(interaction_ids, self.interaction_padding_idx),
+            other=interaction_ids,
         )
         interaction_embedding = self.interaction_embedding(interaction_ids)[:, :-1, :]
 